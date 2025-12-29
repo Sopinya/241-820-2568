f0e0185dd8c628bddbcf1c82091828a850d9862e
Sopinya <sopinya.t@ku.th>
2025-12-29 12:07:21 +0000

    update html

---
+++ main.js
@@ -0,0 +1,39 @@
+&& เเละ
+|| หรือ 
+! not
+*/
+ึlet nunber1 = 5;
+let number2 10;
+//true && true = true
+let  condition1 = (nunber1 >= 3 || number2 >= 10);
+console.log("condition:", condition1);
+
+let age = 30;
+let gender = "ชาย";
+//true && true = true
+if(age > 20 && gender === "ชาย"){
+    console.log("ผู้ชายที่มีอายุ 20 ปีขึ้นไป");
+}
+
+let numder = 20
+ if(numder % 2 ==0){
+    console.log("เลขคู่");
+ }
+
+
+/*
+while statements:
+while
+for
+*/
+ let
+ count = 0;
+ while(count < 10){
+    console.log('Hello world');
+    //countre = countre +1;
+    counter +1;
+ }
+
+ for(let i=0; i<10; i++){
+    console.log('Hello world from for loop');
+ } 
