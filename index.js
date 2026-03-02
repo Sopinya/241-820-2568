332dfabf90b2e2ceaa124013a94a74390f35b2ad
Sopinya <sopinya.t@ku.th>
2026-03-02 18:31:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 8000;
+function submitForm(event) {
 
-app.use(bodyParser.json()); 
+    event.preventDefault();
 
-let users = [];
-let counter = 1;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
 
-// path = /test
-app.get('/test', (req, res) => {
-  let user = {
-    name: 'John Doe',
-    age: 30,
-    email: 'john.doe@example.com'
-  };
-  res.json(user);
-});
+    if (firstName && lastName) {
 
-// path = POST /user
-app.post('/user', (req, res) => {
-  let user = req.body;
-  user.id = counter++; // 
-  users.push(user);
+        alert("ข้อมูลถูกต้อง ✅");
 
-  res.json({
-    message: 'User added successfully',
-    user: user
-  });
-});
+        // เด้งไปหน้า phpMyAdmin
+        window.location.href = "http://localhost/phpmyadmin";
 
-// path = PUT /user/:id
-app.patch('/user/:id', (req, res) => {
-  const id = req.params.id; 
-  const updateUser = req.body; 
+    } else {
 
-  // หา user ที่มี id ตรงกับ id ที่ส่งมา
-  let selectedIndex = users.findIndex(user => user.id == id);
+        alert("กรุณากรอกข้อมูลให้ครบ ❌");
 
-  if (updateUser.name) {
-    users[selectedIndex].name = updateUser.name;
-  }
-  if (updateUser.age) {
-    users[selectedIndex].age = updateUser.age;
-  }
-
-  // อัปเดต user ที่เจอ
-  users[selectedIndex].name = updateUser.name || users[selectedIndex].name;
-  users[selectedIndex].age = updateUser.age || users[selectedIndex].age;  
-
-
-  res.json({
-    message: 'User updated successfully',
-    data: {
-      user: users[selectedIndex],
-      indexUpdated: selectedIndex
     }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+}
